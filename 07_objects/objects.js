d06d104b67bae0cf90bc2a048a6e37578a55ee8c
Anastasiia <anastasiiadudka1996@gmail.com>
2024-07-17 08:19:23 -0400

    homework

--- 07_objects/objects.js
+++ 07_objects/objects.js
@@ -1,13 +1,4 @@
-// 
-class employee {
-    name = 'Hayat';
-    age = 28;
-    salary = 80000;
-    jobTitle = 'Manager';
-};
 
-   work () 
-    console.log(`${employee}is working hard`)
    
   
 
