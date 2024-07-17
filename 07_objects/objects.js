f1596942b000173d6d30e06c4bec895dbb623c4f
Anastasiia <anastasiiadudka1996@gmail.com>
2024-07-17 08:11:59 -0400

    hw

---
+++ 07_objects/objects.js
@@ -0,0 +1,13 @@
+// 
+class employee {
+    name = 'Hayat';
+    age = 28;
+    salary = 80000;
+    jobTitle = 'Manager';
+};
+
+   work () 
+    console.log(`${employee}is working hard`)
+   
+  
+
---
+++ 08_spread/spread.js
@@ -0,0 +1,50 @@
+//Write a function that takes in two arrays of numbers and returns a new array containing all the elements of both arrays
+
+//let arrayOne = [1,2,3];
+//let arrayTwo = [3,4,5];
+//let combinedArray = [...arrayOne,...arrayTwo];
+//console.log(combinedArray);
+
+//Write a function that takes in an array of strings and a string to add to the beginning of the array, and returns a new array with the string added to the beginning.
+
+//const array1 = ["apple,","cherry"];
+//const array2 = "mango";
+//const result = addStringtoArray(array1,array2);
+//function addStringtoArray(array, string){
+//    return [string, ...array];
+//}
+//console.log(result);
+
+
+//Write a function that takes in an object and a key-value pair to add to the object, and returns a new object with the key-value pair added.
+//const originalObject = {name: "Ana", age: 32};
+//const newKey = "name2";
+//const newValue = "David";
+//const newObject = {...originalObject,[newKey]:newValue};
+//console.log(newObject);
+
+
+//Write a function that takes in an array of numbers and returns the largest number in the array.
+//const numberArray = [5,8,9,3,24,19];
+//function findLargestNumber(array){
+//    if (array.length ===0){
+//       throw new Errow("Array is empty");
+//    }
+//    return Math.max(...array);
+//}
+//const largestNumber = findLargestNumber(numberArray);
+//console.log(largestNumber);
+
+
+//Write a function that takes in an array of strings and returns a new array containing all the strings in alphabetical order.
+
+//const originalArray = ['banana','apple','cherry','strawberry','blueberry'];
+//function alphabeticalOrder(array){
+//   const newArray = array.slice();
+//   newArray.sort()
+ //  return newArray;
+//}
+//const originalSortedArray = alphabeticalOrder(originalArray);
+//console.log(originalSortedArray);
+
+
